1893244a2a583ccbf175a05e63435e2f42ea9a93
youyou <93966747+lisa3456@users.noreply.github.com>
2023-09-15 05:20:26 +0800

    Update ikjqr.js

--- ikjqr.js
+++ ikjqr.js
@@ -25,6 +25,7 @@ try {
 	let html = request(input, {
         headers: {
 			// 'User-Agent':'PC_UA',
+            Referer: '',
             'User-Agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
         }
     });
